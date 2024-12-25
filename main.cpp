baaa189496f9b025e02858ae8eef5c9bb2de2415
Prapti Gawande <prapting2812@gmail.com>
2024-12-25 17:49:42 +0530

    inisialisations

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include "parser.h"
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+const string TRACE = "trace";
+const string SHOW_STATISTICS = "stats";
+const string ALGORITHMS[9] = {"", "FCFS", "RR-", "SPN", "SRT", "HRRN", "FB-1", "FB-2i", "AGING"};
+
+bool sortByServiceTime(const tuple<string, int, int> &a, const tuple<string, int, int> &b)
+{
+    return (get<2>(a) < get<2>(b));
+}
+bool sortByArrivalTime(const tuple<string, int, int> &a, const tuple<string, int, int> &b)
+{
+    return (get<1>(a) < get<1>(b));
+}
+bool descendingly_by_response_ratio(tuple<string, double, int> a, tuple<string, double, int> b)
+{
+    return get<1>(a) > get<1>(b);
+}
+bool byPriorityLevel (const tuple<int,int,int>&a,const tuple<int,int,int>&b){
+    if(get<0>(a)==get<0>(b))
+        return get<2>(a)> get<2>(b);
+    return get<0>(a) > get<0>(b);
+}//if priority level are same sort by service time
+
+void clear_timeline()
+{
+    for(int i=0; i<last_instant; i++)
+        for(int j=0; j<process_count; j++)
+            timeline[i][j] = ' ';
+}
+
+string getProcessName(tuple<string, int, int> &a)
+{
+    return get<0>(a);
+}
+
+int getArrivalTime(tuple<string, int, int> &a)
+{
+    return get<1>(a);
+}
+
+int getServiceTime(tuple<string, int, int> &a)
+{
+    return get<2>(a);
+}
+
+int getPriorityLevel(tuple<string, int, int> &a)
+{
+    return get<2>(a);
+}
+
+double calculate_response_ratio(int wait_time, int service_time)
+{
+    return (wait_time + service_time)*1.0 / service_time;
+}
+
+void fillInWaitTime(){
+    for (int i = 0; i < process_count; i++)
+    {
+        int arrivalTime = getArrivalTime(processes[i]);
+        for (int k = arrivalTime; k < finishTime[i]; k++)
+        {
+            if (timeline[k][i] != '*')
+                timeline[k][i] = '.';
+        }
+    }
+}
