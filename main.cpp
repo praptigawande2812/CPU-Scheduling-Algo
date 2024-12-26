66caff7aa385183c3e5480444a34b8a8dde7d4af
Prapti Gawande <prapting2812@gmail.com>
2024-12-26 12:21:00 +0530

    rr,hrrn

--- main.cpp
+++ main.cpp
@@ -71,93 +71,186 @@ void fillInWaitTime(){
     }
 }
 
-//FCFS
+// First-Come, First-Serve Scheduling (FCFS)
 void firstComeFirstServe()
 {
-    int time = getArrivalTime(processes[0]);
-    for (int i = 0; i < process_count; i++)
-    {
+    int time = getArrivalTime(processes[0]); // Start at the arrival time of the first process
+    for (int i = 0; i < process_count; i++) {
         int processIndex = i;
         int arrivalTime = getArrivalTime(processes[i]);
         int serviceTime = getServiceTime(processes[i]);
 
-        finishTime[processIndex] = (time + serviceTime);
-        turnAroundTime[processIndex] = (finishTime[processIndex] - arrivalTime);
-        normTurn[processIndex] = (turnAroundTime[processIndex] * 1.0 / serviceTime);
+        // Calculate finish time, turnaround time, and normalized turnaround time
+        finishTime[processIndex] = time + serviceTime;
+        turnAroundTime[processIndex] = finishTime[processIndex] - arrivalTime;
+        normTurn[processIndex] = turnAroundTime[processIndex] * 1.0 / serviceTime;
 
         for (int j = time; j < finishTime[processIndex]; j++)
             timeline[j][processIndex] = '*';
         for (int j = arrivalTime; j < time; j++)
             timeline[j][processIndex] = '.';
+
+        // Update current time after completing the process
         time += serviceTime;
     }
 }
 
-//SJF
+// Shortest Job First Scheduling (SJF)
 void shortestJobFirst()
 {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // pair of service time and index
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // Min-heap based on service time
     int j = 0;
-    for (int i = 0; i < last_instant; i++)
-    {
-        while(j<process_count && getArrivalTime(processes[j]) <= i){
+    for (int i = 0; i < last_instant; i++) {
+        // Add all processes arriving at or before the current time
+        while (j < process_count && getArrivalTime(processes[j]) <= i) {
             pq.push(make_pair(getServiceTime(processes[j]), j));
             j++;
         }
-        if (!pq.empty())
-        {
-            int processIndex = pq.top().second;
+
+        if (!pq.empty()) {
+            int processIndex = pq.top().second; // Process with shortest service time
             int arrivalTime = getArrivalTime(processes[processIndex]);
             int serviceTime = getServiceTime(processes[processIndex]);
             pq.pop();
 
-            int temp = arrivalTime;
-            for (; temp < i; temp++)
+            for (int temp = arrivalTime; temp < i; temp++)
                 timeline[temp][processIndex] = '.';
 
-            temp = i;
-            for (; temp < i + serviceTime; temp++)
+            for (int temp = i; temp < i + serviceTime; temp++)
                 timeline[temp][processIndex] = '*';
 
-            finishTime[processIndex] = (i + serviceTime);
-            turnAroundTime[processIndex] = (finishTime[processIndex] - arrivalTime);
-            normTurn[processIndex] = (turnAroundTime[processIndex] * 1.0 / serviceTime);
-            i = temp - 1;
+            // Calculate finish time, turnaround time, and normalized turnaround time
+            finishTime[processIndex] = i + serviceTime;
+            turnAroundTime[processIndex] = finishTime[processIndex] - arrivalTime;
+            normTurn[processIndex] = turnAroundTime[processIndex] * 1.0 / serviceTime;
+
+            i += serviceTime - 1; // Skip to the end of the process execution
         }
     }
 }
 
-//SRTF
+// Shortest Remaining Time First Scheduling (SRTF)
 void shortestRemainingTimeFirst()
 {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // Min-heap based on remaining time
     int j = 0;
-    for (int i = 0; i < last_instant; i++)
-    {
-        while(j<process_count &&getArrivalTime(processes[j]) == i){
+    for (int i = 0; i < last_instant; i++) {
+        // Add all processes arriving at the current time
+        while (j < process_count && getArrivalTime(processes[j]) == i) {
             pq.push(make_pair(getServiceTime(processes[j]), j));
             j++;
         }
-        if (!pq.empty())
-        {
+
+        if (!pq.empty()) {
             int processIndex = pq.top().second;
             int remainingTime = pq.top().first;
             pq.pop();
             int serviceTime = getServiceTime(processes[processIndex]);
             int arrivalTime = getArrivalTime(processes[processIndex]);
+
             timeline[i][processIndex] = '*';
 
-            if (remainingTime == 1) // process finished
-            {
+            // If the process finishes, calculate times; otherwise, update remaining time
+            if (remainingTime == 1) {
                 finishTime[processIndex] = i + 1;
-                turnAroundTime[processIndex] = (finishTime[processIndex] - arrivalTime);
-                normTurn[processIndex] = (turnAroundTime[processIndex] * 1.0 / serviceTime);
-            }
-            else
-            {
+                turnAroundTime[processIndex] = finishTime[processIndex] - arrivalTime;
+                normTurn[processIndex] = turnAroundTime[processIndex] * 1.0 / serviceTime;
+            } else {
                 pq.push(make_pair(remainingTime - 1, processIndex));
             }
         }
     }
     fillInWaitTime();
+}
+
+// Round Robin Scheduling (RR)
+void roundRobin(int originalQuantum)
+{
+    queue<pair<int, int>> q; // Queue to store process index and remaining service time
+    int j = 0;
+
+    // Add the first process if it arrives at time 0
+    if (getArrivalTime(processes[j]) == 0) {
+        q.push(make_pair(j, getServiceTime(processes[j])));
+        j++;
+    }
+
+    int currentQuantum = originalQuantum;
+
+    for (int time = 0; time < last_instant; time++) {
+        if (!q.empty()) {
+            int processIndex = q.front().first;
+            int &remainingServiceTime = q.front().second; 
+            int arrivalTime = getArrivalTime(processes[processIndex]);
+            int serviceTime = getServiceTime(processes[processIndex]);
+
+            timeline[time][processIndex] = '*';
+            remainingServiceTime--;
+            currentQuantum--;
+
+            // Check if the process has finished or quantum expired
+            if (remainingServiceTime == 0) {
+                finishTime[processIndex] = time + 1;
+                turnAroundTime[processIndex] = finishTime[processIndex] - arrivalTime;
+                normTurn[processIndex] = turnAroundTime[processIndex] * 1.0 / serviceTime;
+                q.pop();
+                currentQuantum = originalQuantum;
+            } else if (currentQuantum == 0) {
+                q.pop();
+                q.push(make_pair(processIndex, remainingServiceTime));
+                currentQuantum = originalQuantum;
+            }
+        }
+
+        // Add new processes arriving at the current time
+        while (j < process_count && getArrivalTime(processes[j]) == time + 1) {
+            q.push(make_pair(j, getServiceTime(processes[j])));
+            j++;
+        }
+    }
+
+    fillInWaitTime();
+}
+
+// Highest Response Ratio Next Scheduling (HRRN)
+void highestResponseRatioNext()
+{
+    vector<tuple<string, double, int>> present_processes; // Tuple: <process_name, response_ratio, time_in_service>
+    int j = 0;
+    for (int i = 0; i < last_instant; i++) {
+        // Add processes arriving at or before the current time
+        while (j < process_count && getArrivalTime(processes[j]) <= i) {
+            present_processes.push_back(make_tuple(getProcessName(processes[j]), 1.0, 0));
+            j++;
+        }
+
+        // Update response ratios for all ready processes
+        for (auto &proc : present_processes) {
+            string process_name = get<0>(proc);
+            int process_index = processToIndex[process_name];
+            int wait_time = i - getArrivalTime(processes[process_index]);
+            int service_time = getServiceTime(processes[process_index]);
+            get<1>(proc) = calculate_response_ratio(wait_time, service_time);
+        }
+
+        // Sort processes by descending response ratio
+        sort(all(present_processes), descendingly_by_response_ratio);
+
+        if (!present_processes.empty()) {
+            int process_index = processToIndex[get<0>(present_processes[0])];
+            while (i < last_instant && get<2>(present_processes[0]) != getServiceTime(processes[process_index])) {
+                timeline[i][process_index] = '*';
+                i++;
+                get<2>(present_processes[0])++;
+            }
+            i--;
+            present_processes.erase(present_processes.begin());
+
+            // Calculate finish time, turnaround time, and normalized turnaround time
+            finishTime[process_index] = i + 1;
+            turnAroundTime[process_index] = finishTime[process_index] - getArrivalTime(processes[process_index]);
+            normTurn[process_index] = turnAroundTime[process_index] * 1.0 / getServiceTime(processes[process_index]);
+        }
+    }
+    fillInWaitTime(); 
 }
